d9799516ca201918b6d8ef61a5667d6429b24734
Sorrell123 <multibrunio@gmail.com>
2016-07-25 17:23:02 +0200

    Added Corriculum Vitae Section

--- js/main.js
+++ js/main.js
@@ -3,4 +3,17 @@ $(document).ready(function(){
 	progress(80, $(".high"));
 	progress(60, $(".mid"));
 	progress(40, $(".low"));
+
+	// Pojawianie sie elementów wraz ze scrollem
+	$(".section").addClass("opacity0").viewportChecker({
+		classToAdd: "opacity1 animated fadeIn",
+		offset: 150
+	});
+
+
+	//CV toggles
+	$(".toggleButton").click(function(){
+		$(this).next().toggle("slow");
+		$(this).children().toggleClass("none");
+	});
 });
