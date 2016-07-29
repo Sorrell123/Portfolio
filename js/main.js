e1fc8e373dd7399a4d639e1ebdc7d4adf4145676
Sorrell123 <multibrunio@gmail.com>
2016-07-29 22:24:37 +0200

    Changed scroll menu and finished work on skill loading bars

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 $(document).ready(function(){
 	//loading bars
-	progress(80, $(".high"));
-	progress(60, $(".mid"));
-	progress(40, $(".low"));
+	$(".skill").addClass("opacity0").viewportChecker({
+		classToAdd: "opacity1 animated fadeIn",
+		offset: 150,
+		callbackFunction: function(){
+			progress(80, $(".cycle-slide-active > div > div.high"));
+			progress(60, $(".cycle-slide-active > div > div.mid"));
+			progress(40, $(".cycle-slide-active > div > div.low"));
+		}
+	});
+
+	$(".skillControllButton").click(function(){
+		progress(80, $(".cycle-slide-active > div > div.high"));
+		progress(60, $(".cycle-slide-active > div > div.mid"));
+		progress(40, $(".cycle-slide-active > div > div.low"));
+	});
 
 	// Pojawianie sie elementów wraz ze scrollem
 	$(".section").addClass("opacity0").viewportChecker({
 		classToAdd: "opacity1 animated fadeIn",
 		offset: 150
 	});
-
 
 	//CV toggles
 	$(".toggleButton").click(function(){
@@ -31,62 +42,68 @@ $(document).ready(function(){
 
 
 	//Give to menu its funcionality
-	$(".home").click(function() {
-
-	    $(".headerContainer").ScrollTo({
+	$(".home").click(function(event) {
+		event.preventDefault();
+	    $("#header").ScrollTo({
 	    	duration: 2000,
     	    easing: 'linear'
 	    });
 	});
 
-	$(".about").click(function() {
+	$(".about").click(function(event) {
+		event.preventDefault();
 	    $('html, body').animate({
-	        scrollTop: $(".firstSection").offset().top
+	        scrollTop: $("#first").offset().top
 	    }, 1000);
 	});
 
-	$(".skills").click(function() {
+	$(".skills").click(function(event) {
+		event.preventDefault();
 	    $('html, body').animate({
-	        scrollTop: $(".secondSection").offset().top
+	        scrollTop: $("#second").offset().top
 	    }, 1000);
 	});
 
-	$(".cv").click(function() {
+	$(".cv").click(function(event) {
+		event.preventDefault();
 	    $('html, body').animate({
-	        scrollTop: $(".thirdSection").offset().top
+	        scrollTop: $("#third").offset().top
 	    }, 1000);
 	});
 
-	$(".portfolio").click(function() {
+	$(".portfolio").click(function(event) {
+		event.preventDefault();
 	    $('html, body').animate({
-	        scrollTop: $(".fourthSection").offset().top
+	        scrollTop: $("#fourth").offset().top
 	    }, 1000);
 	});
 
-	$(".contact").click(function() {
-		$('html, body').animate({
-	        scrollTop: $(".fifthSection").offset().top
+	$(".contact").click(function(event) {
+		event.preventDefault();
+	    $('html, body').animate({
+	        scrollTop: $("#fifth").offset().top
 	    }, 1000);
 	});
 
 	//Pushing menu button
 	$(".menuButton").click(function(){
-		$(".menuScroll").toggle("slow");
+		$(".menuScroll").slideToggle("slow");
+		$(".menuArrowDown").toggleClass("none");
+		$(".menuArrowUp").toggleClass("none");
 	});
 
 	//Showing menu button when scroll is lower
-	
 	$(document).scroll(function() {
         var navHeight = $(".menu").height();
         var scrollAmount = $(window).scrollTop();
-
-        console.log(scrollAmount);
 
         if(scrollAmount > navHeight){
         	$(".menuButton").removeClass("none");
         }else if(scrollAmount <= navHeight){
         	$(".menuButton").addClass("none");
         	$(".menuScroll").css("display", "none");
+        	$(".menuArrowDown").removeClass("none");
+        	$(".menuArrowUp").addClass("none");
         }
 	});
 	
@@ -96,10 +113,5 @@ $(document).ready(function(){
 		$(".menuScroll").css("display", "block");
 		event.stopPropagation();
 	});
-	*/
-
-	//some things here
-
-	
-	
+	*/	
 });
--- js/progressBar.js
+++ js/progressBar.js
@@ -1,4 +1,15 @@
 function progress(percent, $element) {
 	var progressBarWidth = percent * $element.width() / 100;
-	$element.find("div").animate({width: progressBarWidth }, 1000).html(percent + "%");
-}
+	$element.find("div").animate({
+		width: progressBarWidth 
+	}, 1000).html(percent + "%");
+};
+
+/*
+function progress(percent) {
+	var progressBarWidth = percent * $(this).width() / 100;
+	$(this).find("div").animate({
+		width: progressBarWidth 
+	}, 1000).html(percent + "%");
+};
+*/
